5353e78160c084130022c9c4195af9dadd46f5d5
Pavani Bayikati <161501559+PavaniBayikati@users.noreply.github.com>
2024-11-05 23:33:22 +0530

    Update Logincontroller.java

--- Logincontroller.java
+++ Logincontroller.java
@@ -6,6 +6,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 @WebServlet("/registerController1")
 public class registerController1 extends HttpServlet {
+  
 private static final long serialVersionUID = 1L;
 protected void doPost(HttpServletRequest request,
 HttpServletResponse response) throws ServletException,
